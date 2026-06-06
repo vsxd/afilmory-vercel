6c2b4000ee86919480cf22ae97c5ea6430c28a66
vsxd <sun.xd@hotmail.com>
2026-06-06 18:38:02 +0800

    fix: clear production audit vulnerabilities

--- apps/web/src/atoms/route.ts
+++ apps/web/src/atoms/route.ts
@@ -21,7 +21,6 @@ export const [routeAtom, , , , getReadonlyRoute, setRoute] = createAtomHooks(
       hash: "",
       state: null,
       key: "",
-      unstable_mask: undefined,
     },
   }),
 );
